c1ee47ba25fcba13c3e90ecf6697c55202111639
ehrtho <64816364+ehrtho@users.noreply.github.com>
2022-03-14 11:50:06 -0300

    Create struct.lua

---
+++ src/struct.lua
@@ -0,0 +1,29 @@
+
+address = function(default)
+
+end
+
+bignumber = function(default)
+
+end
+
+number = function(default)
+
+end
+
+boolean = function(default)
+
+end
+
+string = function(default)
+
+end
+
+array = function(default)
+
+end
+
+function struct(struct_type)
+
+
+end
